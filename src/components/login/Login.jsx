ed87d57d4ce6263392cef47bead2926f120f2d1a
Mohammad Ali <always.mohammad.ali@gmail.com>
2025-09-14 20:27:40 +0600

    Sign in & out done with toggle system

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,20 +1,58 @@
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-import React from 'react';
+import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+import React, { useState } from 'react';
 import { auth } from '../firebase/firebase.init';
 
 
 
 const Login = () => {
-    const provider = GoogleAuthProvider();
 
-    const handleLogin = () =>{
-        signInWithPopup(auth, provider)
-        console.log('sign is starting')
+    const provider = new GoogleAuthProvider();
+    const [user, setUser] = useState(null)
+
+    const handleSignIn = () =>{
+        
+
+        console.log('signin is starting');
+
+        signInWithPopup(auth, provider).then(result =>{
+            console.log(result.user);
+            setUser(result.user);
+
+        })
+        .catch(error =>{
+            console.log(error);
+        })
     }
+    const handleSignOut = () =>{
+          signOut(auth).then( () =>{
+            console.log('Sign out done')
+            setUser(null)
+          })
+          .catch(error =>{
+            console.log(error)
+          })
+    }
+
     return (
         <div>
             <h1 className='mb-5'>Please Sign in</h1>
-            <button onClick={handleLogin} className='bg-green-200 p-2 rounded-lg'>Sign/Log in with google</button>
+            
+            
+
+            {
+                user ?
+                 <button onClick={handleSignOut} className='bg-green-200 p-2 rounded-lg'>Sign Out</button>
+                  :
+                 <button onClick={handleSignIn} className='bg-green-200 p-2 rounded-lg'>Sign In</button>
+            }
+
+            {
+                user && <div className=''>
+                  <h1>{user.displayName}</h1>
+                  <p>{user.email}</p>
+                  <img src={user.photoURL} alt="" />
+                </div>
+            }
         </div>
     );
 };
